bbb7b7d48b43cb29ea22f40da32a6a4239e50117
Renan Muniz <renanmuni1999@gmail.com>
2023-03-15 21:48:06 -0300

    creation of project and mysql connection with flyway

---
+++ GerenciamentoFinanceiro-API/src/main/java/com/example/financeiro/api/GerenciamentoFinanceiroApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.financeiro.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoFinanceiroApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoFinanceiroApiApplication.class, args);
+	}
+
+}
