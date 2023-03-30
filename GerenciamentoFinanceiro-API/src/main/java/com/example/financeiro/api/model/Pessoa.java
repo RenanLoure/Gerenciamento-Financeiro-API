0f79db323ca19c88416739b0bb81d23f091a912a
Renan Muniz <renanmuni1999@gmail.com>
2023-03-29 23:24:33 -0300

    implementing person

--- GerenciamentoFinanceiro-API/src/main/java/com/example/financeiro/api/model/Pessoa.java
+++ GerenciamentoFinanceiro-API/src/main/java/com/example/financeiro/api/model/Pessoa.java
@@ -1,11 +1,15 @@
 package com.example.financeiro.api.model;
 
 import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 
+@Entity
+@Table(name = "pessoa")
 public class Pessoa {
 	
 	@Id
