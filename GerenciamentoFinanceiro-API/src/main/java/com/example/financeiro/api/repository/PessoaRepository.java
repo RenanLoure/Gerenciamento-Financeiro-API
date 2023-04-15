f9a22fac3f62e4c547f3538abbdbcb01ccfdf421
Renan Muniz <renanmuni1999@gmail.com>
2023-04-14 21:21:45 -0300

    add lancamento validations

--- GerenciamentoFinanceiro-API/src/main/java/com/example/financeiro/api/repository/PessoaRepository.java
+++ GerenciamentoFinanceiro-API/src/main/java/com/example/financeiro/api/repository/PessoaRepository.java
@@ -1,13 +1,10 @@
 package com.example.financeiro.api.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.financeiro.api.model.Pessoa;
 
 public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
 
-	void save(Optional<Pessoa> pessoaSalva);
 
 }
--- GerenciamentoFinanceiro-API/src/main/java/com/example/financeiro/api/resource/LancamentoResource.java
+++ GerenciamentoFinanceiro-API/src/main/java/com/example/financeiro/api/resource/LancamentoResource.java
@@ -1,12 +1,16 @@
 package com.example.financeiro.api.resource;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,8 +19,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.financeiro.api.event.RecursoCriadoEvent;
+import com.example.financeiro.api.expeceptionHandler.ApiFinanceiroException.Erro;
 import com.example.financeiro.api.model.Lancamento;
 import com.example.financeiro.api.repository.LancamentoRepository;
+import com.example.financeiro.api.service.LancamentoService;
+import com.example.financeiro.api.service.exception.PessoaInexistenteOuInativaException;
 
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
@@ -32,25 +39,40 @@ public class LancamentoResource {
 	@Autowired
 	private ApplicationEventPublisher publisher;
 	
+	@Autowired
+	private MessageSource messageSource;
+
+	@Autowired
+	private LancamentoService lancamentoService;
+	
 	@GetMapping
 	public List<Lancamento> listar() {
 		return lancamentoRepository.findAll();
 	}
 	
 	@GetMapping("/{codigo}")
-	public ResponseEntity<Optional<Lancamento>> buscarPeloCodigo(@PathVariable Long codigo) {
+	public ResponseEntity<Optional <Lancamento>> buscarPeloCodigo(@PathVariable Long codigo) {
 		Optional<Lancamento> lancamento = lancamentoRepository.findById(codigo);
 		return !lancamento.isEmpty() ? ResponseEntity.ok(lancamento) : ResponseEntity.notFound().build();
 	}
 	
 	@PostMapping
 	public ResponseEntity<Lancamento> criar(@Valid @RequestBody Lancamento lancamento, HttpServletResponse response) {
-		Lancamento lancamentoSalvo =  lancamentoRepository.save(lancamento);
+		Lancamento lancamentoSalvo =  lancamentoService.salvar(lancamento);
 		
 		publisher.publishEvent(new RecursoCriadoEvent(this, response, lancamentoSalvo.getCodigo()));
 		
 	    return ResponseEntity.status(HttpStatus.CREATED).body(lancamentoSalvo);
 	    
 	}
+	
+	@ExceptionHandler({ PessoaInexistenteOuInativaException.class})
+	public ResponseEntity<Object> handlePessoaInexistenteOuInativaException(PessoaInexistenteOuInativaException ex){
+		String mensagemUsuario = messageSource.getMessage("pessoa.inexistente-ou-inativa", null, LocaleContextHolder.getLocale());
+		String mensagemDev = ex.toString();
+		List<Erro>  erros = Arrays.asList(new Erro (mensagemUsuario, mensagemDev));
+		return ResponseEntity.badRequest().body(erros);
+	}
+	
 	
 }
